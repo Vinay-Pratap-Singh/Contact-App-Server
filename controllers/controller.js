23d7f359de53ec8db6c0b8820bd2a8f85305273e
Vinay Pratap Singh <technicalharvi@gmail.com>
2022-11-21 22:45:42 +0530

    update and delete using id

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,3 @@
-const { findOneAndDelete } = require("../model/contact");
 const Contact = require("../model/contact");
 
 // add the contact
@@ -6,54 +5,97 @@ exports.addContact = async (req, res) => {
   const { name, phone } = req.body;
   const checkPhone = await Contact.findOne({ phone });
 
-  if (!name || !phone) res.send("Please fill both the fields");
-  else if (checkPhone) res.send("Number already exists");
-  else {
+  if (!name || !phone) {
+    res.json({
+      status: 400,
+      message: "Please fill all the required fields",
+    });
+  } else if (checkPhone) {
+    res.json({
+      status: 400,
+      message: "Number already exists",
+    });
+  } else if (phone.toString().length !== 10) {
+    res.json({
+      status: 400,
+      message: "Number length should be 10",
+    });
+  } else {
     try {
-        const newContact = new Contact({ name, phone });
-        await newContact.save();
-        res.send("Contact Created Succesfully");
-      }
-    catch (error) {
+      const newContact = new Contact({ name, phone });
+      await newContact.save();
+      res.send("Contact Created Succesfully");
+    } catch (error) {
       res.send("Contact Creation Failed");
     }
   }
 };
 
-
 // delete the contact
 exports.deleteContact = async (req, res) => {
-  
   try {
-    const { phone } = req.body;
-    const deletePhone = await Contact.findOneAndDelete({ phone });
-    if (deletePhone) res.send("Number Deleted Succesfully");
-    else res.send("Number Not Found");
+    const { id } = req.body;
+    const deletePhone = await Contact.findByIdAndDelete({ _id:id });
+    if (deletePhone) {
+      res.json({
+        status: 200,
+        message: "Number Deleted Succesfully",
+      });
+    } else {
+      res.json({
+        status: 400,
+        message: `Number ${phone} Not Found`,
+      });
+    }
   } catch (error) {
-    res.status(500).send("Deletion Failed\n", error);
+    res.json({
+      status: 500,
+      message: "Deletion Failed",
+      error: error,
+    });
   }
-}
+};
 
 // display all the contact
 exports.displayContact = async (req, res) => {
   try {
     const data = await Contact.find();
-    res.send(data);
+    res.json({
+      status: 200,
+      data,
+    });
   } catch (error) {
-    res.send("Failed to display contact\n",error);
+    res.json({
+      status: 500,
+      message: "Failed to display data",
+    });
   }
-}
+};
 
 // update an existing contact
 exports.updateContact = async (req, res) => {
   try {
-    const { existingContact, name, phone } = req.body;
-    
-    const updatedContact = await Contact.findOneAndUpdate({ phone: existingContact }, { name, phone });
-    
-    if(updatedContact) res.send("Contact Updated Succesfully");
-    else res.send("Contact Updation Failed");
+    const { id, name, phone } = req.body;
+
+    const updatedContact = await Contact.findByIdAndUpdate({_id:id},{name,phone});
+
+    if (updatedContact) {
+      res.json({
+        status: 200,
+        message:"Contact Updated Succesfully"
+      })
+    }
+    else {
+      res.json({
+        status: 500,
+        message:"Contact Deletion Failed"
+      })
+    }
   } catch (error) {
-    res.send(error);
+    res.json({
+      status: 500,
+      message: "Contact Deletion Failed",
+      error
+    })
   }
-}
+};
