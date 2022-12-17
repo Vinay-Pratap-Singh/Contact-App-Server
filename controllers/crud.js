d4da428d5d3d9bbd31a93746853e1bdb2844aa8e
Vinay Pratap Singh <technicalharvi@gmail.com>
2022-12-17 23:05:19 +0530

    added crud functionality for contact

--- controllers/crud.js
+++ controllers/crud.js
@@ -9,8 +9,8 @@ exports.addContact = async (req, res) => {
   if (!name || !phone) {
     return res.status(400).json({
       success: false,
-      message:"All fields are mandatory"
-    })
+      message: "All fields are mandatory",
+    });
   }
 
   let myUser;
@@ -25,13 +25,13 @@ exports.addContact = async (req, res) => {
 
   // if user does not exist
   if (!myUser) {
-   return res.status(200).json({
+    return res.status(200).json({
       success: false,
       message: "User not found",
-    })
+    });
   }
 
-  // creating the new contact 
+  // creating the new contact
   const newContact = { name, phone };
 
   // adding the new contact to the user list
@@ -42,17 +42,149 @@ exports.addContact = async (req, res) => {
     await myUser.save();
     return res.status(200).json({
       success: true,
-      message:"New contact created"
-    })
+      message: "New contact created",
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message:"Failed to save new contact"
-    })
+      message: "Failed to save new contact",
+    });
   }
 };
 
 // function to delete the individual contact
 exports.deleteContact = async (req, res) => {
-  
-}
+  // getting the id of user
+  const id = req.id;
+
+  // getting the id of number to be deleted
+  const { deleteId } = req.body;
+
+  // checking that the user exist in db or not
+  let myUser;
+  try {
+    myUser = await User.findById({ _id: id });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "User not found",
+      error,
+    });
+  }
+
+  // if user does not exist
+  if (!myUser) {
+    return res.status(200).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  // finding the contact number to delete
+  const newContactArray = myUser.contact;
+  let index = undefined;
+  for (let i = 0; i < newContactArray.length; i++) {
+    if (newContactArray[i]._id == deleteId) {
+      index = i;
+      break;
+    }
+  }
+
+  // if not found
+  if (index === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: "Contact does not exist",
+    });
+  }
+
+  // if exist then deleting that one
+  newContactArray.splice(index, 1);
+
+  try {
+    // updating the data in database
+    myUser.contact = newContactArray;
+    await myUser.save();
+
+    // sending back the sucessfull response
+    return res.status(200).json({
+      success: true,
+      message: "Contact deleted successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "failed to delete contact",
+      error,
+    });
+  }
+};
+
+// function to update the existing contact
+exports.updateContact = async (req, res) => {
+  // getting the id of user
+  const id = req.id;
+
+  // getting the id, to update the field
+  const { updateId, name, phone } = req.body;
+
+  // checking that the user exist in db or not
+  let myUser;
+  try {
+    myUser = await User.findById({ _id: id });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "User not found",
+      error,
+    });
+  }
+
+  // if user does not exist
+  if (!myUser) {
+    return res.status(200).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  // finding the contact number to update
+  const newContactArray = myUser.contact;
+  let index = undefined;
+  for (let i = 0; i < newContactArray.length; i++) {
+    if (newContactArray[i]._id == updateId) {
+      index = i;
+      break;
+    }
+  }
+
+  // if not found
+  if (index === undefined) {
+    return res.status(400).json({
+      success: false,
+      message: "Contact does not exist",
+    });
+  }
+
+  // if exist then updating that one
+  newContactArray[index].name = name;
+  newContactArray[index].phone = phone;
+
+  try {
+    // updating the data in database
+    myUser.contact = newContactArray;
+    await myUser.save();
+
+    // sending back the sucessfull response
+    return res.status(200).json({
+      success: true,
+      message: "Contact updated successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "failed to update contact",
+      error,
+    });
+  }
+};
--- route/contactRoute.js
+++ route/contactRoute.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { signup, login } = require("../controllers/account");
-const { addContact } = require("../controllers/crud");
+const { addContact, deleteContact, updateContact } = require("../controllers/crud");
 const { dashboard } = require("../controllers/dashboard");
 const { auth } = require("../middleware/auth");
 const Router = express.Router();
 
-Router.post("/addcontact",auth, addContact)
-// Router.post("/deletecontact", deleteContact)
-// Router.post("/updatecontact", updateContact)
-Router.post("/signup", signup)
-Router.post("/login",login)
+Router.post("/addcontact", auth, addContact);
+Router.delete("/deletecontact", auth, deleteContact);
+Router.patch("/updatecontact",auth, updateContact)
+Router.post("/signup", signup);
+Router.post("/login", login);
 Router.get("/dashboard", auth, dashboard);
 
 module.exports = Router;
