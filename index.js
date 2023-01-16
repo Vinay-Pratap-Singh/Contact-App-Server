400561fb711f1b9783a2762bbbccd5f903c87493
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-01-16 15:43:48 +0530

    fixing token

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ cloudinary.config({
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://contact-app-harvi.netlify.app',
     credentials:true
 }));
 app.use(cookieParser());
