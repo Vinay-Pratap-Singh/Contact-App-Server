fd1e3b97fb282a6c40d13b4894e6c09c2f6cd06d
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-01-16 12:05:26 +0530

    deployment fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ cloudinary.config({
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     credentials:true
 }));
 app.use(cookieParser());
